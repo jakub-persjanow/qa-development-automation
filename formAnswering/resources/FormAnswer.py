02c114b5c8f26649ae6a89d1bd736f280efb7a61
dgela <demian.gela@llinformatics.com>
2025-07-10 12:03:07 +0200

    11_07

---
+++ formAnswering/resources/FormAnswer.py
@@ -0,0 +1,4 @@
+class FormAnswer:
+    def __init__(self, name, email) -> None:
+        self.name = name
+        self.email = email
---
+++ formAnswering/tests/FormKeywords.py
@@ -0,0 +1,5 @@
+from ..resources.Form import Form
+
+def get_form_object(header_text, name_header_text, email_header_text):
+    form_object = Form(header_text=header_text, name_header_text=name_header_text, email_header_text=email_header_text)
+    return form_object
